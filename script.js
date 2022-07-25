9c1c40c87ca9f20d8392dcd98f6f81be80e85464
Brayan Bernal <brayanbernal0710@gmail.com>
2022-07-24 20:20:13 -0400

    Updates

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const ACCESS = (localStorage.getItem('access') === true);
 
 console.log(ACCESS,window.location.hostname);
-return;
-if (!ACCESS && !window.location.pathname.includes('login')) {
+
+if (!ACCESS && !window.location.href.includes('login')) {
     console.log("Está pasando por la autenticación");
     localStorage.setItem('access',false);
     window.location.replace(window.location.hostname+"/livecar/login");
