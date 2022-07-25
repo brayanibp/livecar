4ca338ced5da6016388d2168a57dcd8ea9896664
Brayan Bernal <brayanbernal0710@gmail.com>
2022-07-24 20:25:08 -0400

    Updates

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(ACCESS,window.location.hostname);
 if (!ACCESS && !window.location.href.includes('login')) {
     console.log("Está pasando por la autenticación");
     localStorage.setItem('access',false);
-    window.location.href("https://"+window.location.hostname+"/livecar/login");
+    window.location.assign("https://"+window.location.hostname+"/livecar/login");
 }
 
 let menubar = document.querySelector('#menu-bars');
