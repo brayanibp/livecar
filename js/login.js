b910730c927e760396cf663db6fe92e7ae4782e5
Brayan Bernal <brayanbernal0710@gmail.com>
2022-07-25 16:43:58 -0400

    Updates

--- js/login.js
+++ js/login.js
@@ -11,7 +11,7 @@ const sendData = async (event) => {
   const res = await fetch('https://livecarapi.herokuapp.com/login', {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     method: 'POST',
-    body: JSON.stringify(body)
+    body: body
   });
   const data = await res.json();
   console.log(data, JSON.stringify(body));
