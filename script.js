27859d6d45dccaeb5f67d9a39798cf84a793efa7
Brayan Bernal <brayanbernal0710@gmail.com>
2022-07-24 20:18:28 -0400

    Updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const ACCESS = (localStorage.getItem('access') === true);
 
 console.log(ACCESS,window.location.hostname);
-
+return;
 if (!ACCESS && !window.location.pathname.includes('login')) {
     console.log("Está pasando por la autenticación");
     localStorage.setItem('access',false);
