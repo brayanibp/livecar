8ea136c9bd8a85ff7fdcee8fc14a0f2f75e4b149
Brayan Bernal <brayanbernal0710@gmail.com>
2022-07-31 00:16:49 -0400

    Updates

--- js/initialScript.js
+++ js/initialScript.js
@@ -1,7 +1,4 @@
-const crypted = CryptoJS.AES.encrypt("aaaaaaa","secret").toString();
 const SESSION = getAuthToken();
-
-console.log(SESSION,window.location.hostname, crypted);
 
 if (!SESSION && !window.location.href.includes('login')) {
     localStorage.setItem('SESSION',null);
