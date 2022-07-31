003821d08db4c96bad07cefe5f64e44d04fee10c
Brayan Bernal <brayanbernal0710@gmail.com>
2022-07-31 17:44:27 -0400

    Updates

--- js/clientes.js
+++ js/clientes.js
@@ -1,33 +1,47 @@
 // CLIENTES
 //ADD CLIENTE
-const $create_client_form = document.querySelector('#form-add-clien');
+const $create_client_form = document.querySelector('#form-add-client');
 if ($create_client_form) {
     $create_client_form.addEventListener('submit', async (event) => {
         event.preventDefault();
+        const $error_text = document.querySelector('#client-error-text') || null;
         const formDataClient = new FormData(event.currentTarget);
         const client = formDataToJSON(formDataClient);
+        console.log(client);
         try {
-          const res = await fetch('/cliente/create', {
-              method: 'POST',
-              body: client,
+          const res = await axios.post('https://livecarapi.herokuapp.com/cliente/create', {
+              client: client
           });
           console.log(res);
-        } catch (error) {
-          console.log(error);
+        } catch (err) {
+          console.log(err);
+          const { response } = err;
+          $error_text.innerHTML = response.data.message;
         }
     })
 }
 
-// COSULTAR / ELIMNAR CLIENTE 
-const $form_con_el_clientes = document.querySelector('#form-con-mod-clientes')
-if ($form_con_el_clientes) {
-    $form_con_el_clientes.addEventListener('submit', (event) => {
-        event.preventDefault()
-        const formDataConElCliente = new formData(event.currentTarget)
-        fetch('/', {
-            method: 'POST',
-            body: formDataConElCliente,
-        })
+// COSULTAR / ELIMINAR CLIENTE 
+const $form_get_client = document.querySelector('#form-get-client');
+if ($form_get_client) {
+    $form_get_client.addEventListener('submit', (event) => {
+        event.preventDefault();
+    });
+    const $get_client = document.querySelector('#get_client');
+    const $delete_client = document.querySelector('#delete_client');
+    $get_client.addEventListener('click', async ()=>{
+      const formDataClient = new FormData($form_get_client);
+      const clientIDs = formDataToJSON(formDataClient);
+      console.log(clientIDs);
+      try {
+        const res = await axios.get(`https://livecarapi.herokuapp.com/cliente/${clientIDs.id_cliente || clientIDs.cedula_cliente}`);
+        console.log(res);
+      } catch (error) {
+        
+      }
+    });
+    $delete_client.addEventListener('click',()=>{
+      console.log("CLICK2")
     })
 }
 
