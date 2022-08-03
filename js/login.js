2a2ceebe114f87fcf4c8a9e7b48dd2e16eb00c84
Brayan Bernal <brayanbernal0710@gmail.com>
2022-08-03 09:10:57 -0400

    Updates

--- js/login.js
+++ js/login.js
@@ -1,7 +1,6 @@
 const $form_login = document.querySelector('#form-login');
 
-const sendData = async (event) => {
-  event.preventDefault();
+const sendData = async () => {
   const $form_login = document.querySelector('#form-login') || null;
   const formDataLogin = new FormData($form_login);
   const login = formDataToJSON(formDataLogin);
@@ -23,6 +22,26 @@ const sendData = async (event) => {
 
 if ($form_login) {
   $form_login.addEventListener('submit',(ev)=>{
-    sendData(ev);
-  })
+    ev.preventDefault();
+  });
+  const $login_button = document.querySelector('login_button');
+  $login_button.addEventListener('click',()=>{
+    sendData();
+  });
+  const $reset_button = document.querySelector('#reset_password');
+  $reset_button.addEventListener('click', async () => {
+    const formDataLogin = new FormData($form_login);
+    const { email } = formDataToJSON(formDataLogin);
+    const $error_text = document.querySelector('#error-text') || null;
+    try {
+      const res = await axios.post('https://livecarapi.herokuapp.com/reset_password', {
+        email
+      });
+      console.log(res);
+      alert(`Se ha enviado un correo a: ${email} con los nuevos datos. Revisa tu bandeja principal o la sección de SPAM.`);
+    } catch (error) {
+      const { response } = error;
+      $error_text.innerHTML = response.data.message;
+    }
+  });
 }
