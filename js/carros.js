ab7b438d6ff00654accdae984e38ccc061b92f35
Brayan Bernal <brayanbernal0710@gmail.com>
2022-08-02 23:58:40 -0400

    Updates

--- js/carros.js
+++ js/carros.js
@@ -13,6 +13,7 @@ if ($form_con_el_car) {
       const $response_text = document.querySelector('#carro-get-response');
       const $error_text = document.querySelector('#carro-get-error-text');
       try {
+        console.log(carroIDs);
         const res = await axios.get(`https://livecarapi.herokuapp.com/carro/${carroIDs.ID || 'mat/'+carroIDs.matricula}`, {
           carro: carroIDs,
         });
